9b1e4523cb86f2d8d83731059409d6f464382f78
davidlenze <davidlenze@yahoo.de>
2016-09-23 17:39:45 +0200

    Die neue index.js die auf dem Rasperry läuft!

--- local_server/index.js
+++ local_server/index.js
@@ -1,14 +1,45 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var tempIt = require('./temp.js');
+
+// Globally stored temperature data
+var temperatures = [];
+
+// Pad temperature values with zeroes, in case there's not actually enough data
+// available
+function padValues(values) {
+  if (values.length < 20) {
+    for (var i = 0; i < (20 - values.length); i++) {
+      values.push(0);
+    }
+  }
+
+  return values;
+}
+
 
 app.get('/temperatur.csv', function(req, res) {
-  array = [];
-  for(var i = 0; i < 20; i++) {
-    array.push(Math.floor(Math.random() * 25 + 10));
-  }
-  res.send(array.join(','));
+  res.send(temperatures.join(','));
+});
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(3000, function() {
-  console.log('Der Nicotinamidadenindinucleotid-Server läuft auf Port 3000!');
+	console.log('Der Visualisierungs-Server läuft auf Port 3000!');
+  
+ 
+  function processResults(results) {
+    temperatures = padValues(results);
+    console.log('Neue Temperaturdaten wurden geladen:', temperatures);
+  }
+
+  tempIt(processResults);
+
+
+
 });
+
